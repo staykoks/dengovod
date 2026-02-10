eef812e8d310f1eb70c43b9385bba8ce33627cc6
staykoks <93823902+staykoks@users.noreply.github.com>
2026-02-10 04:28:51 +0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///finance.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'dev-secret')
