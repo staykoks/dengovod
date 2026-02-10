408c36b3a2ec3d4faa0d8aabba7d713fc74f9aee
staykoks <93823902+staykoks@users.noreply.github.com>
2026-02-10 04:41:16 +0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'dev-secret')
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'jwt-secret')
